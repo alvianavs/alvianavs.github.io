e73e6ffeb84e9217c8767746a8a4302099d87753
Avis Alvian <avisalvian44@gmail.com>
2021-07-05 17:23:14 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      lobster: ['Lobster'],
+      bree: ['Bree Serif'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
